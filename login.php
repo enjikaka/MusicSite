6a8c81079ce1d7a6e46bacdd8e9a7582aa50d027
Jeremy Karlsson <jeremy@enji.se>
2014-11-25 18:32:31 +0100

    Fixed typos, whitespace and HTML5

--- login.php
+++ login.php
@@ -1,34 +1,24 @@
 <?php
 	$title="Login";
 	include("incl/header.php");
-	
 ?>
-					<div id="content">
-					
-						<h1><?php echo($title); ?></h1>
-						<hr />
-						
-						<?php ?>
-						
-						<fieldset>
-						  <legend>Type username and password</legend>
-						   <form action="login.php" method="post" name="frmLogin" >
-								<label>
-									Name
-									<br />
-									<input type="text" name="txtUserName" id="txtUserName" title="Username" placeholder="Type your username!" autofocus="autofocus" required="required" />
-								</label>
-								<br />
-								<label>
-									Password
-									<br />
-									<input type="password" name="txtPassWord" id="txtPassWord" title="Password" placeholder="Type your Password!" required="required" />
-								</label>
-								<br />
-								<input type="submit" name="btnLogin" id="btnLogin" value="Login" />
-								<input type="reset" name="btnReset" id="btnReset" value="Reset" />
-						  </form>
-						</fieldset>
-					</div>
+
+<div id="content">
+	<h1><?php echo($title); ?></h1>
+	<hr>
+	<fieldset>
+		<legend>Type username and password</legend>
+		<form action="login.php" method="post" name="frmLogin" >
+			<label for="txtUserName">Name</label><br>
+			<input type="text" name="txtUserName" id="txtUserName" placeholder="Type your username!" autofocus required>
+			<br>
+			<label for="txtPassWord">Password</label><br>
+			<input type="password" name="txtPassWord" id="txtPassWord" placeholder="Type your password!" required>
+			<br>
+			<button type="submit" name="btnLogin" id="btnLogin">Login</button>
+			<button type="reset" name="btnReset" id="btnReset">Reset</button>
+		</form>
+	</fieldset>
+</div>
 
 <?php include("incl/footer.php");
