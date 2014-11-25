c6288567f0cfa2f358eb673a3411045bd423c1c0
Jeremy Karlsson <jeremy@enji.se>
2014-11-25 18:33:21 +0100

    Rm some files. Fixed whitespace in php.

--- logout.php
+++ logout.php
@@ -2,13 +2,10 @@
 	$title="Logout";
 	include("incl/header.php");
 ?>
-				<div id="content">
-					
-					<h1> You are no longer logged on!</h1>
-					<hr />
 
-                </div>
+<div id="content">
+  <h1>You are no longer logged on!</h1>
+  <hr>
+</div>
 
-<?php 
-	include("incl/footer.php");
-?>
+<?php include("incl/footer.php"); ?>
