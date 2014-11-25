291d1ed91668ec74b7740c733579d17609364ad9
Jeremy Karlsson <jeremy@enji.se>
2014-11-25 18:46:41 +0100

    fixing

--- incl/footer.php
+++ incl/footer.php
@@ -1,54 +1,42 @@
+<nav>
+	<ul>
+		<?php
+			//if(isset($admin) && ($admin == "secretpage"))
+			//{
+			include("incl/adminmenu.php");
+			//}
+			//else
+			//{
+			include("incl/mainmenu.php");
+			//}
+		?>
+	</ul>
+</nav>
+</div><!-- end main -->
+<footer>Kontaktinformation</footer>
+</div><!-- end wrapper -->
 
-					<nav>
-						<ul>
-							<?php 
-								//if(isset($admin) && ($admin == "secretpage"))
-								//{
-									include("incl/adminmenu.php"); 
-								//}
-								//else
-								//{
-									include("incl/mainmenu.php");
-								//}
-							?>
-						</ul>
-					</nav>
-					
-					
-				</div><!-- end main -->
-				
-                <footer>
-                    Kontaktinformation
-                </footer>
-
-            </div><!-- end wrapper -->
-
-			<?php
-				if(isset($jquery)) {
-				?>
-					<script type="text/javascript" src="jquery/jquery-2.1.1.js"></script>
-				<?php
-				}
-				
-				if(isset($slimbox)) {
-				?>
-					<script type="text/javascript" src="slimbox2/js/slimbox2.js"></script>
-				<?php
-				}
-				
-				if(isset($accordion)) {
-				?>
-					<script type="text/javascript" src="jquery-ui-1.11.1.custom/jquery-ui.js"></script>
-				<?php
-				}
-				
-				if(isset($script)) {
-				?>
-					<script type="text/javascript" src="script/<?php echo($script); ?>"></script>
-				<?php
-				}
-			?>
-			
-        </body>
-
-    </html>
+<?php
+	if(isset($jquery)) {
+?>
+	<script src="jquery/jquery-2.1.1.js"></script>
+<?php
+	}
+	if(isset($slimbox)) {
+?>
+	<script src="slimbox2/js/slimbox2.js"></script>
+<?php
+	}
+	if(isset($accordion)) {
+?>
+	<script src="jquery-ui-1.11.1.custom/jquery-ui.js"></script>
+<?php
+	}
+	if(isset($script)) {
+?>
+	<script src="script/<?php echo($script); ?>"></script>
+<?php
+	}
+?>
+</body>
+</html>
--- incl/header.php
+++ incl/header.php
@@ -1,41 +1,27 @@
-<?php 
+<?php
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 ?>
-<!doctype html>
-    <html lang="en">
-
-        <head>
-
-            <meta charset="utf-8" />
-			<link href="style/stilmall.css" rel="stylesheet" type="text/css" />
-			
-			<?php
-				
-				if(isset($accordion)) {
-				?>
-					<link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.css" />
-				<?php
-				}
-	
-				if(isset($slimbox)) {
-				?>
-					<link rel="stylesheet" href="slimbox2/css/slimbox2.css" />
-				<?php
-				}
-				
-			?>
-			
-            
-            <title><?php echo($title); ?></title>
-
-        </head>
-
-        <body>
-			<div id="wrapper">
-			
-				<header>
-					My MusicSite
-				</header>
-				
-				<div id="main">
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link href="style/stilmall.css" rel="stylesheet">
+    <?php
+      if(isset($accordion)) {
+		?>
+    <link rel="stylesheet" href="jquery-ui-1.11.1.custom/jquery-ui.css" />
+		<?php
+      }
+      if(isset($slimbox)) {
+    ?>
+      <link rel="stylesheet" href="slimbox2/css/slimbox2.css" />
+    <?php
+      }
+    ?>
+    <title><?php echo($title); ?></title>
+  </head>
+<body>
+<div id="wrapper">
+<header>My MusicSite</header>
+<div id="main">
